de48ce91fc900b08db42a44ad6b2c39d5195a40c
Manoel Ribeiro <manoelribeiro20181@gmail.com>
2021-04-24 21:59:25 -0300

    Corrigurando classe startup para mapear conexão do banco SQLite

--- EventosBackEnd/Eventos.API/Startup.cs
+++ EventosBackEnd/Eventos.API/Startup.cs
@@ -1,7 +1,9 @@
+using Eventos.API.Persistence;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,7 +28,7 @@ namespace Eventos.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<EventosDbContext>(options => options.UseSqlite(Configuration.GetConnectionString("Conexao")));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -40,6 +42,7 @@ namespace Eventos.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Eventos.API v1"));
             }
