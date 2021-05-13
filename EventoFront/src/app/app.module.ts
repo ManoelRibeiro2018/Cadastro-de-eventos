ab3b36a4f5887d93b24e7bd78be486f08b272566
manoel ribeiro <manoelribeiro20181@gmail.com>
2021-05-13 12:13:49 -0300

    Criando components Cadastro e login

--- EventoFront/src/app/app.module.ts
+++ EventoFront/src/app/app.module.ts
@@ -23,6 +23,9 @@ import { DasboardComponent } from './components/dasboard/dasboard.component';
 import { TituloComponent } from './shared/Titulo/Titulo.component';
 import { EventoDetalheComponent } from './components/eventos/evento-detalhe/evento-detalhe.component';
 import { EventoListaComponent } from './components/eventos/evento-lista/evento-lista.component';
+import { UserComponent } from './components/user/user.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { RegistrationComponent } from './components/user/registration/registration.component';
 
 
 @NgModule({
@@ -36,7 +39,10 @@ import { EventoListaComponent } from './components/eventos/evento-lista/evento-l
     TituloComponent,
       NavComponent,
       EventoDetalheComponent,
-      EventoListaComponent
+      EventoListaComponent,
+      UserComponent,
+      LoginComponent,
+      RegistrationComponent
    ],
   imports: [
     BrowserModule,
