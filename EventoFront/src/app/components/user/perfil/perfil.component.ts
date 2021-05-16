49a53f74b3699b2a75b512d3f3ef21cfc0b3c2eb
manoel ribeiro <manoelribeiro20181@gmail.com>
2021-05-15 21:55:42 -0300

    Criando validação da tela de perfil

--- EventoFront/src/app/components/user/perfil/perfil.component.ts
+++ EventoFront/src/app/components/user/perfil/perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorPassword } from '@app/helpers/ValidatorPassword';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  form: FormGroup;
+   get f(){
+    return this.form.controls;
   }
 
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.validation();
+  }
+    private validation(): void{
+      const formOptions: AbstractControlOptions ={
+        validators: ValidatorPassword.MusMatch('senha', 'confirmaSenha')
+        }
+      this.form = this.fb.group({
+        titulo: ['', [Validators.required], Validators.minLength(4)],
+        primeiroNome: ['', [Validators.required], Validators.minLength(4)],
+        ultimoNome: ['', [Validators.required], Validators.minLength(4)],
+        email: ['', [Validators.required, Validators.email]],
+        telefone: ['', Validators.required],
+        funcao: ['', Validators.required],
+        descricao: ['', Validators.required],
+        senha: ['', [Validators.required], Validators.minLength(4)],
+        confirmaSenha: ['', Validators.required],
+      },formOptions)
+    }
 }
